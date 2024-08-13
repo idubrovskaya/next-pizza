11f7686e79ccab3fb10acdd9f59af19a7c829318
Irina Dubrovskaya <irina.dubrovskaya95@gmail.com>
2024-08-13 15:34:43 +0300

    add search component, add api for products

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -4,6 +4,8 @@ import { Container } from './container';
 import Image from 'next/image';
 import { Button } from '../ui';
 import { ArrowRight, ShoppingCart, User } from 'lucide-react';
+import Link from 'next/link';
+import { SearchInput } from '.';
 
 interface IHeaderProps {
   className?: string;
@@ -14,14 +16,20 @@ export const Header: React.FC<IHeaderProps> = ({ className }) => {
     <header className={cn('border border-b', className)}>
       <Container className='flex items-center justify-between py-8'>
         {/* Левая часть */}
-        <div className='flex items-center gap-4'>
-          <Image src='/logo.png' alt='logo' width={35} height={35} />
-          <div>
-            <h1 className='text-2xl uppercase font-black'>Next Pizza</h1>
-            <p className='text-sm text-gray-400 leading-3'>
-              вкусней уже некуда
-            </p>
+        <Link href='/'>
+          <div className='flex items-center gap-4'>
+            <Image src='/logo.png' alt='logo' width={35} height={35} />
+            <div>
+              <h1 className='text-2xl uppercase font-black'>Next Pizza</h1>
+              <p className='text-sm text-gray-400 leading-3'>
+                вкусней уже некуда
+              </p>
+            </div>
           </div>
+        </Link>
+
+        <div className='mx-10 flex-1'>
+          <SearchInput />
         </div>
 
         {/* Правая часть */}
