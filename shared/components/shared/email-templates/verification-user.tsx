75d98fc2359b7aec407db8cbc235a2cbd56b1e2a
Irina Dubrovskaya <irina.dubrovskaya95@gmail.com>
2024-09-24 14:32:34 +0300

    changed web url

--- shared/components/shared/email-templates/verification-user.tsx
+++ shared/components/shared/email-templates/verification-user.tsx
@@ -11,7 +11,9 @@ export const VerificationUserTemplate: React.FC<Props> = ({ code }) => (
     </p>
 
     <p>
-      <a href={`http://localhost:3000/api/auth/verify?code=${code}`}>
+      <a
+        href={`https://next-pizza-neon.vercel.app/api/auth/verify?code=${code}`}
+      >
         Подтвердить регистрацию
       </a>
     </p>
